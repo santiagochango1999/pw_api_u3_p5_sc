e64509cfa449ab6a05c02a60c86b490c9f41f65d
Santiago Chango <sachango@uce.edu.ec>
2024-01-27 10:53:37 -0500

    taller27

--- src/main/java/com/example/demo/PwApiU3P5ScApplication.java
+++ src/main/java/com/example/demo/PwApiU3P5ScApplication.java
@@ -1,13 +1,33 @@
 package com.example.demo;
 
+import org.hibernate.type.descriptor.java.LocalDateTimeJavaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PwApiU3P5ScApplication {
+import com.example.demo.repository.modelo.Estudiante;
+import com.example.demo.service.IEstudianteService;
 
+@SpringBootApplication
+public class PwApiU3P5ScApplication implements CommandLineRunner{
+
+	@Autowired
+	private IEstudianteService estudianteService;
 	public static void main(String[] args) {
 		SpringApplication.run(PwApiU3P5ScApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		/*Estudiante e1=new Estudiante();
+		e1.setNombre("santiago");
+		e1.setApellido("apellido");
+		e1.setGenero("M");
+		e1.setFechaNacimiento(null);
+		
+		this.estudianteService.guardar(e1);*/
+	}
 }
+	
--- src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
+++ src/main/java/com/example/demo/controller/EstudianteControllerRestFul.java
@@ -1,5 +1,41 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.repository.modelo.Estudiante;
+import com.example.demo.service.IEstudianteService;
+
+//API: por el proyecto java
+//API: viene determinado por un proyecto y puede tener muchos servicios y muchas capacidades
+//el servicio es anlogo a decir el controller y estos dos se les implementa a travez de una clase controller
+
+@RestController//servicio
+@RequestMapping(path="/estudiantes") //se le da el nombre del servicio
 public class EstudianteControllerRestFul {
 
+	@Autowired
+	private IEstudianteService estudianteService;
+	
+	// Metodos= Capacidades
+	//GET
+	
+	@GetMapping(path = "/buscar")
+	public Estudiante buscar() {
+		return this.estudianteService.buscar(1);
+	}
+	
+	//http://localhost:8080/API/v1.0/Matricula/estudiantes/buscar
+	
+	
+	@PostMapping(path = "/guardar")
+	public void guardar(@RequestBody Estudiante estudiante) {
+		this.estudianteService.guardar(estudiante);
+	}
+	
+	//http://localhost:8080//API/v1.0/Matricula/estudiantes/guardar
 }
